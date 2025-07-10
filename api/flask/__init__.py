a4635c6e20ee1de90b9df3dec1420ceb47dd84cc
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2025-07-10 00:58:56 -0400

    Update __init__.py

--- api/flask/__init__.py
+++ api/flask/__init__.py
@@ -298,5 +298,5 @@ def serve_annotated_image(filename):
 
 
 if __name__ == '__main__':
-    app.run(port=5001)
-    # app.run(host="0.0.0.0", port=5001)
+    # app.run(port=5001)
+    app.run(host="0.0.0.0", port=5001)
